9ab77a83ad3fb004c9e910a840609e64ac0aefd0
Ingo-Rößner <ingo.spicker@gmail.com>
2014-07-30 13:39:13 +0200

    fixture count

--- DataFunction.h
+++ DataFunction.h
@@ -3,30 +3,14 @@
 
 #include "LTestAssert.h"
 #include "sstream"
+#include <type_traits>
 
 using namespace std;
 
-template<class T>
-string argsToString(T arg){
-    string result;
-    stringstream ss;
-    ss<<arg;
-    result = ss.str();
-    return result;
-}
-
-template<class T, class... Types>
-string argsToString(T arg, Types&&... args){
-    string result;
-    stringstream ss;
-    ss<<arg<<", ";
-    result = ss.str();
-    result = result + argsToString(args...);
-    return result;
-}
 
 class DataFunctionBase{
 public:
+    unsigned int count;
     virtual bool isVoidReturn(){
         return false;
     }
@@ -38,14 +22,28 @@ class DataFunction: public DataFunctionBase{
 public:
     typedef ReturnType(*FooType)(ParameterTypes...);
     FooType foo;
+
     DataFunction(FooType f){
         foo = f;
+        count = 0;
     }
 
     template <typename T, class... Types>
     void run(T ret, Types&&... args){
-        ReturnType result = foo(args...);
-        LTAssert::Equal(result, ret, "Fail at fixture("+argsToString(args...)+") -> "+argsToString(ret)+"!="+argsToString(result));
+        ++count;
+        ReturnType result;
+        stringstream sstm;
+        string msg;
+        try{
+            result = foo(args...);
+        }catch(...){
+            msg = "Exception at fixture ";
+            sstm << msg << count;
+            throw LTAssert::FalseAssert(sstm.str());
+        }
+        msg = "Fail at fixture ";
+        sstm << msg << count;
+        LTAssert::Equal(result, ret, sstm.str());
     }
 };
 
@@ -54,13 +52,23 @@ class DataFunction<void, ParameterTypes...>: public DataFunctionBase{
 public:
     typedef void(*FooType)(ParameterTypes...);
     FooType foo;
+
     DataFunction(FooType f){
         foo = f;
+        count = 0;
     }
 
     template <class... Types>
     void run(Types&&... args){
-        foo(args...);
+        ++count;
+        try{
+            foo(args...);
+        }catch(...){
+            string msg = "Exception at fixture ";
+            stringstream sstm;
+            sstm << msg << count;
+            throw LTAssert::FalseAssert(sstm.str());
+        }
     }
 
     bool isVoidReturn(){
