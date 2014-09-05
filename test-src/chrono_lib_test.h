7c37b7ae575080e7475fc998b8d48fa54e2f94dd
Dan Häberlein <dan.haeberlein@googlemail.com>
2014-09-06 00:06:14 +0200

    added test-src folder with chrone c++ library test and new goal to build self tests in code blocks project

---
+++ test-src/chrono_lib_test.h
@@ -0,0 +1,47 @@
+/*
+ *     The MIT License (MIT)
+ *
+ *     Copyright (c) 2014 Ingo Rössner
+ *                        Dan Häberlein
+ *
+ *     Permission is hereby granted, free of charge, to any person obtaining a copy
+ *     of this software and associated documentation files (the "Software"), to deal
+ *     in the Software without restriction, including without limitation the rights
+ *     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *     copies of the Software, and to permit persons to whom the Software is
+ *     furnished to do so, subject to the following conditions:
+ *
+ *     The above copyright notice and this permission notice shall be included in
+ *     all copies or substantial portions of the Software.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *     THE SOFTWARE.
+ */
+
+#ifndef CHRONO_LIB_TEST
+#define CHRONO_LIB_TEST
+
+#include <chrono>
+#include <thread>
+
+constexpr int SLEEPING_TIME = 500;
+
+TestSuite chrono_suite = {
+
+    LTest::addTest("using chrono library of c++ to measure the time", [](){
+        std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
+        std::this_thread::sleep_for(std::chrono::milliseconds(SLEEPING_TIME));
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end-start;
+        auto millsec = std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed_seconds).count();
+        LTAssert::Equal<double>(millsec / 1e6, SLEEPING_TIME, 1.0);
+    })
+};
+
+#endif // CHRONO_LIB_TEST
---
+++ test-src/run-ltest-tests.cpp
@@ -0,0 +1,6 @@
+#include "../src/LTest.h"
+#include "chrono_lib_test.h"
+
+int main(){
+    LTest::run();
+}
