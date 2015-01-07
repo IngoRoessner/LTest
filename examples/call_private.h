dbf4863263a86730b414ad51e3c361c87f9ae713
Ingo Rößner <ingo.spicker@gmail.com>
2015-01-07 15:37:39 +0100

    fix encoding in licence

--- examples/call_private.h
+++ examples/call_private.h
@@ -1,8 +1,8 @@
 /*
  *     The MIT License (MIT)
  *
- *     Copyright (c) 2014 Ingo R�ssner
- *                        Dan H�berlein
+ *     Copyright (c) 2014 Ingo Rössner
+ *                        Dan Häberlein
  *
  *     Permission is hereby granted, free of charge, to any person obtaining a copy
  *     of this software and associated documentation files (the "Software"), to deal
